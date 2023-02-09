b39142a61d842def30d58a0db94eaf5ce60a35fb
Glitch (glitch-blank-website) <none>
2023-02-09 15:26:38 +0000

    ⛩🏄 Checkpoint ./script.js:49251604/3503

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ if (navigator.mediaDevices.getUserMedia) {
     });
 }
 
-document.getElementById("timer").innerHTML =
+document.getElementById('timer').innerHTML =
   5 + ":" + 0;
 startTimer();
 
 
 function startTimer() {
-  var presentTime = document.getElementById("timer").innerHTML;
+  var presentTime = document.getElementById('timer').innerHTML;
   var timeArray = presentTime.split(/[:]+/);
   var m = timeArray[0];
   var s = checkSecond((timeArray[1] - 1));
-  if(s==59){m=m-4}
+  if(s==59){m=m-1}
   if(m<0){
     return
   }
   
-  document.getElementById("timer").innerHTML =
+  document.getElementById('timer').innerHTML =
     m + ":" + s;
   console.log(m)
   setTimeout(startTimer, 1000);
@@ -33,8 +33,7 @@ function startTimer() {
 }
 
 function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; 
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
   if (sec < 0) {sec = "59"};
   return sec;
 }
-
