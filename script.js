a2345001db4022dce06b3718861f8bb6aa5ff5f0
Glitch (glitch-blank-website) <none>
2023-02-09 13:54:17 +0000

    🏢🍃 Checkpoint ./script.js:49251604/624 ./micro.html:49251604/14 ./style.css:49251604/25

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ if (navigator.mediaDevices.getUserMedia) {
       console.log("Something went wrong!");
     });
 }
+
+document.getElementById("timer").innerHTML =
+  5 + ":" + 0;
+startTimer();
+
+
+function startTimer() {
+  var presentTime = document.getElementById("timer").innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-4}
+  if(m<0){
+    return
+  }
+  
+  document.getElementById("timer").innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+  
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
