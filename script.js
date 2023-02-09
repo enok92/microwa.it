6531c2e21faff26a087d4a318db76091452bdb6e
Glitch (glitch-blank-website) <none>
2023-02-09 15:15:34 +0000

    🌊🍐 Checkpoint ./micro.html:49251604/337 ./script.js:49251604/38

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ function startTimer() {
 }
 
 function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; 
   if (sec < 0) {sec = "59"};
   return sec;
 }
+
